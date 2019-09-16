89fbaad37207ba3e0357b6f4d2be05f56a8a36d9
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-09-16 12:03:33 -0400

    set up routes for getting, creating, updating

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,5 +3,36 @@ const burger = require("../models/burger");
 const router = express.Router();
 
 router.get("/", function(req, res) {
-
+    burger.all(function(data) {
+        let hbsObject = {
+            burgers: data
+        };
+        res.render("index", hbsObject);
+    })
 });
+
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    let devoured = "id = " + req.params.id; 
+
+    burger.update({
+        devoured: req.body.devroured
+    }, devoured, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router; 
