7586ed4a591d42a4a640fc55bd2d330c8478b35e
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-09-18 11:21:40 -0400

    added app.js with ajax

---
+++ public/assets/js/app.js
@@ -0,0 +1,38 @@
+$(function() {
+    $(".devoured").on("click", function(e) {
+        let id =$(this).data("id");
+        let devourNow = $(this).data("devournow");
+
+        let newDevouredState = {
+            devoured: devourNow
+        };
+
+    $.ajax("/api/burgers" + id, {
+        type: "PUT", 
+        data: newDevouredState
+    }).then (
+        function() {
+            location.reload();
+        }
+    );
+    $("#addburger").empty();
+});
+
+    $(".create-burger").on("submit", function(event) {
+        event.preventDefault(); 
+
+        let newBurger = {
+            name: $("#addburger").val().trim(), 
+            devoured: 0
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST", 
+            data: newBurger
+        }).then(
+            function() {
+                location.reload();
+            }
+        );
+    });
+});
